82d998518da32fa7d8d01265c82d04cd1f16bb25
bistik <erickbianes@gmail.com>
2026-08-11 20:23:40 +0800

    get a_tag links and img_tag src urls

--- crawl.py
+++ crawl.py
@@ -1,4 +1,4 @@
-from urllib.parse import urlsplit
+from urllib.parse import urlsplit, urljoin
 from bs4 import BeautifulSoup, Tag
 
 def normalize_url(url_str: str) -> str:
@@ -19,3 +19,23 @@ def get_first_paragraph_from_html(html: str) -> str:
     if p_tag is not None:
         return p_tag.get_text(strip=True)
     return ''
+
+def get_urls_from_html(html, base_url):
+    soup = BeautifulSoup(html, "html.parser")
+    a_tags = soup.find_all('a')
+    urls = []
+    for a_tag in a_tags:
+        url = a_tag.get('href')
+        if url:
+            urls.append(urljoin(base_url, url))
+    return urls
+
+def get_images_from_html(html, base_url):
+    soup = BeautifulSoup(html, "html.parser")
+    img_tags = soup.find_all('img')
+    urls = []
+    for a_tag in img_tags:
+        src = a_tag.get('src')
+        if src:
+            urls.append(urljoin(base_url, src))
+    return urls
